b15468055d0605ac6586e02c137ad7627100311c
Niclas Doll <niclas.doll@amazonis.net>
2022-10-16 21:26:54 +0200

    Upped Package Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="defparse",
-    version="0.1.1",
+    version="0.1.2",
     description="Command Line Arguments from Callable Signatures and Docstrings",
     long_description=open("README.md", "r").read(),
     long_description_content_type='text/markdown',
